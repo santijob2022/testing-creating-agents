8530c49d3d3ce3e6664b0e1976e7d9022a46a79d
Valentin <santi.job.2022@gmail.com>
2025-06-05 17:17:23 -0600

    Initial testing of langmem with qdrant in the react agent

--- AgentsWithMemory/prototype_folder/prompts.py
+++ AgentsWithMemory/prototype_folder/prompts.py
@@ -1,5 +1,5 @@
 # Agent prompt baseline 
-agent_system_prompt = """
+agent_system_prompt_memory = """
 < Role >
 You are {full_name}'s executive assistant. You are a top-notch executive assistant who cares about managing {name}'s job applications.
 </ Role >
@@ -10,6 +10,8 @@ You have access to the following tools to help manage {name}'s communications an
 1. write_email(to, subject, content) - Send emails to specified recipients
 2. schedule_meeting(attendees, subject, duration_minutes, preferred_day) - Schedule calendar meetings
 3. check_calendar_availability(day) - Check available time slots for a given day
+4. manage_memory - Store any relevant information about contacts, actions, discussion, etc. in memory for future reference
+5. search_memory - Search for any relevant information that may have been stored in memory
 </ Tools >
 
 < Instructions >
