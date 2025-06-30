3ef02cfdf09ee3e6ddd30dc5a3bdd43f2f24b2af
Valentin <santi.job.2022@gmail.com>
2025-06-29 23:41:12 -0600

    Starting the backend

---
+++ FullStack/React-FastAPI-LLMs/backend/src/app.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Request, Response
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
+
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
